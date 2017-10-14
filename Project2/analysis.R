b6fdab4841a763078852898ae7cc09bfe38f205c
jdramosf <jd.ramosf@gmail.com>
2017-10-13 20:57:20 -0500

    Restructure analysis file, added analysis to rmd file, deployed shinyapp in server

--- Project2/analysis.R
+++ Project2/analysis.R
@@ -1,25 +1,37 @@
-#prop.table(table(default))
-#prop.table(table(sex))
-
+# Let's transform some variables first so that is easy to work with them later on
 df$marriage <- as.factor(marriage)
 df$sex <- as.factor(sex)
 df$age <- as.integer(as.factor(age))
+df$default <- as.factor(as.logical(df$default))
+
+# Logistic Regression
 
 # Is sex an accurate predictor of credit card default?
 glm.fit = glm(default ~ sex, data = df, family = "binomial")
-#summary(glm.fit)
 glm.probs = predict(glm.fit, type = "response")
-#glm.probs[1:5]
 glm.pred = ifelse(glm.probs < 0.2212, 1, 0)
 #table(glm.pred, default)
 
-#p2 = mosaic(default ~ sex, data = df)
+# Can we use the timing of individuals' payments to predict their default likelihood?
+df$default <- as.numeric(as.logical(df$default))
+
+# Linear Discriminant Analysis
+lda.fit=lda(default~pay_0+pay_2+pay_3, data=df)
+lda.pred=predict(lda.fit)
+df1 = data.frame(lda.pred)
+
+# Quadratic Discriminant Analysis
+qda.fit=qda(default~pay_0+pay_2+pay_3, data=df)
+qda.pred=predict(qda.fit)
+df2 = data.frame(qda.pred)
 
 # K-Nearest Neighbors
 
+# Choosing the relevant variables and creating a subset
 vars <- c("pay_0", "bill_amt1", "pay_amt1")
 df.subset <- df[vars]
 
+# Creating test and train subsets
 test <- 1:9000
 df.train <- df.subset[-test,]
 df.test <- df.subset[test,]
@@ -27,20 +39,9 @@ df.test <- df.subset[test,]
 train.def <- df$default[-test]
 test.def <- df$default[test]
 
+# Fitting the models
 knn.1 <-  knn(df.train, df.test, train.def, k=1)
 knn.5 <-  knn(df.train, df.test, train.def, k=5)
 knn.20 <- knn(df.train, df.test, train.def, k=20)
 knn.100 <- knn(df.train, df.test, train.def, k=100)
 
-#100 * sum(test.def == knn.20)/9000
-
-#prop.table(table(knn.1, test.def),1)
-#prop.table(table(knn.5, test.def),1)
-#prop.table(table(knn.20, test.def),2)
-#table(knn.20, test.def)
-
-#prop.table(table(knn.1, test.def),2)
-#table(knn.1, test.def)
-#table(test.def)
-
-p3 = ggplot(df, aes(x = pay_0, y = bill_amt1, color = as.factor(default))) + geom_jitter()
--- Project2/data.R
+++ Project2/data.R
@@ -1,5 +1,7 @@
 project <- "https://data.world/jdramosf/f-17-eda-project-2"
 data.world::set_config(cfg_env("DW_API"))
-df <- data.world::query(data.world::qry_sql("SELECT * FROM data"), dataset = project)
+df <- data.world::query(
+  data.world::qry_sql("SELECT * FROM data"),
+  dataset = project)
 attach(df)
 summary(df)
